38498cec35ab2ce30d0377c9a335cef40ceeee61
Dhirender pratap singh <division.pvt0@gmail.com>
2024-12-21 13:43:31 +0530

    fix: replaced deprecated code

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 image = None
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_container_width=True)
 
 submit = st.button("Analyze image")
 
